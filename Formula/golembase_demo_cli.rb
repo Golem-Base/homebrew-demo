9dabe5966ee94801cf74d1eba72d46ac7029b208
Dragan Milic <dragan@netice9.com>
2025-04-04 12:09:34 +0200

    upgrade upload artifacts

--- Formula/golembase_demo_cli.rb
+++ Formula/golembase_demo_cli.rb
@@ -1,5 +1,5 @@
-class Golembase < Formula
-  desc "Command line for golembase demo"
+class GolembaseDemoCli < Formula
+  desc "Command-line for golembase demo"
   homepage "https://github.com/Golem-Base/golembase-demo-cli"
   url "https://github.com/Golem-Base/golembase-demo-cli/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "0fddf1abc8f76d22032df08228235d9510ce7944966f9fe3dcfdd246002d04ff"
