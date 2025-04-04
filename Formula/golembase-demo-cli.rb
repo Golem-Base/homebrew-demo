069ea727848f43eb3c1a74617dd0eb66436e9ad2
Dragan Milic <dragan@netice9.com>
2025-04-04 16:13:30 +0200

    latest golembase-demo-cli

--- Formula/golembase-demo-cli.rb
+++ Formula/golembase-demo-cli.rb
@@ -1,8 +1,8 @@
 class GolembaseDemoCli < Formula
   desc "Command-line for golembase demo"
   homepage "https://github.com/Golem-Base/golembase-demo-cli"
-  url "https://github.com/Golem-Base/golembase-demo-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "19c46d232bb4e07b8a5a504e66e7d985900eb7369ea13fe45e88ac1a147cdab6"
+  url "https://github.com/Golem-Base/golembase-demo-cli/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "ede8bb72be9392d113904a8791227dd98415669a10f9a0319347c130525f7b31"
   license "GPL-3.0-or-later"
 
   depends_on "go" => :build
