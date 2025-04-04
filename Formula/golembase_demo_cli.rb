7e226aa56bd08f2e12917f498b951a4eaf4abdf5
Dragan Milic <dragan@netice9.com>
2025-04-04 13:33:59 +0200

    fix otput

--- Formula/golembase_demo_cli.rb
+++ Formula/golembase_demo_cli.rb
@@ -8,7 +8,7 @@ class GolembaseDemoCli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"golembase-demo-cli", *std_go_args(output: nil)
+    system "go", "build", "-o", bin/"golembase-demo-cli", *std_go_args
   end
 
   test do
